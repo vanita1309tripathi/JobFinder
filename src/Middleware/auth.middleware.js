1002c9286241372fe1fe1472864dcbff53000747
vanita1309tripathi <vanita1309tripathi@gmail.com>
2025-04-03 16:57:14 +0530

    New Job Addn only for Recruiters

--- src/Middleware/auth.middleware.js
+++ src/Middleware/auth.middleware.js
@@ -1,6 +1,6 @@
 export const auth=(req,res,next)=>{
    
-    if(req.session.userType=='seeker'){
+    if(req.session.userType === 'seeker' || req.session.userType === 'recruiter'){
         next();
     }
     else{
